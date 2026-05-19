1d554e74381f4727589623cefe593216f8cd5546
Deepikap-2998 <deepikap.2998@gmail.com>
2026-05-19 23:06:02 +0530

    stash

--- git_branch/src/main/java/org/example/test.java
+++ git_branch/src/main/java/org/example/test.java
@@ -5,11 +5,9 @@ public class test {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome test branch!");
-<<<<<<< Updated upstream
-        System.out.println("3rd stash");
-=======
+
         System.out.printf("Hello and welcome test branch!");
         System.out.printf("Hello and welcome test branch!");
->>>>>>> Stashed changes
+
     }
 }
